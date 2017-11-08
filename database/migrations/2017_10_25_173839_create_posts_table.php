b42d1a6eac8c3b7ea5677b7a1c55e56166134157
Jordan Fasabi-Rios <jordanrios.software@gmail.com>
2017-11-08 19:30:45 +0000

    my first comit woop wopp!!!

---
+++ database/migrations/2017_10_25_173839_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 400);
+            $table->string('title', 100);
+            $table->mediumText('body');
+            $table->string('media_link', 400);
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
